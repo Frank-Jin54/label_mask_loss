da64588f42d3592c156f616af6d52436a60eeeb9
frankjin <fjin@chata.ai>
2023-04-20 20:05:33 -0600

    refactor model

--- training.py
+++ training.py
@@ -148,9 +148,9 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, dataclasses_num)
+        self.fc1 = nn.Linear(16 * 5 * 5, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, dataclasses_num)
         self.sf = nn.Softmax()
 
     def forward(self, x):
